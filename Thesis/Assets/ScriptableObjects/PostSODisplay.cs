ef3b46ed90eb7223b19f9747647b4fdcff31f6e8
CrystallineWatchmaker <antoineelliot.oury@gmail.com>
2024-08-02 14:06:54 +0100

    cycling through posts button

--- Thesis/Assets/ScriptableObjects/PostSODisplay.cs
+++ Thesis/Assets/ScriptableObjects/PostSODisplay.cs
@@ -6,15 +6,41 @@ using TMPro;
 
 public class PostSODisplay : MonoBehaviour
 {
-    public PostScriptableObject postSO;
+    
+    //script written with help from ChatGPT
+    
+    
+    public PostScriptableObject[] postSO;
 
     public Image postImage;
     public Image accImage;
 
     public TMP_Text communNote;
+
+
+    public Button scrollOn;
+
+    private int currentIndex = 0;
+
     void Start()
     {
-        postImage.sprite = postSO.postImage;
+        if (postSO.Length > 0)
+        {
+            DisplayPost(postSO[currentIndex]);
+        }
+
+        scrollOn.onClick.AddListener(CycleThroughPosts);
+    }
+
+    private void CycleThroughPosts()
+    {
+        currentIndex = (currentIndex + 1) % postSO.Length;
+        DisplayPost(postSO[currentIndex]);
+    }
+    
+    private void DisplayPost(PostScriptableObject postSO){
+
+    postImage.sprite = postSO.postImage;
         accImage.sprite = postSO.accountImage;
 
         communNote.text = postSO.communityNote.ToString();
